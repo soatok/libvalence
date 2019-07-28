4fe629247462065a02b964565bde139438e51cb6
Soatok Dreamseeker <soatok.dhole@gmail.com>
2019-07-28 04:18:30 -0400

    Update public API

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 "use strict";
 
 module.exports = {
+    Bond: require('./Bond'),
+    Release: require('./lib/Release'),
+    SemVerUpdatePolicy: require('./lib/UpdatePolicies/SemVerUpdatePolicy'),
+    UpdateApplier: require('./lib/UpdateApplier'),
     UpdateFetcher: require('./lib/UpdateFetcher'),
+    UpdatePolicy: require('./lib/UpdatePolicy'),
     UpdateVerifier: require('./lib/UpdateVerifier'),
     Utility: require('./lib/Utility')
 };
-
-/*
-1. Check for updates from valence-updateserver.
-   a. Send the access token (if it exists) and channel (default: public)
-   b. Parse response.
-2. If an update is found, download it and the signature.
-3. Verify the signature against one of the local public keys.
-4. If the signature is valid, either:
-   a. check that this information was committed to the configured cryptographic
-      ledger (Trillian / Chronicle), OR
-   b. defer our commitment check to a trusted third party (or a quorum of
-      multiple third parties).
-5. If the signature and commitment are valid (whether verified locally
-   or deferred), apply the update.
-*/
